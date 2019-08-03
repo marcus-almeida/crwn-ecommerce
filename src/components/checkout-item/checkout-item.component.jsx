cbe9a91f70f68c400f18b10d4d17a28d2601d7ed
Marcus Almeida <malmeida@ardintel.com>
2019-08-02 21:57:50 -0400

    Add session storage and data persistence.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
 import './checkout-item.styles.scss';
 import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
