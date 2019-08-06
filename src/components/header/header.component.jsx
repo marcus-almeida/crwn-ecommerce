5af29f3808d7084ad8455c3fca1c4b0e933320e9
Marcus Almeida <malmeida@ardintel.com>
2019-08-06 19:51:07 -0400

    Add styled components.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,41 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
-import './header.styles.scss';
+// import './header.styles.scss';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
+import {
+	HeaderContainer,
+	LogoContainer,
+	OptionsContainer,
+	OptionLink,
+	OptionDiv
+} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-	<div className='header'>
-		<Link className='logo-container' to='/'>
+	<HeaderContainer>
+		<LogoContainer to='/'>
 			<Logo className='logo' />
-		</Link>
-		<div className='options'>
-			<Link className='option' to='/shop'>
+		</LogoContainer>
+		<OptionsContainer>
+			<OptionLink to='/shop'>
 				SHOP
-			</Link>
-			<Link className='option' to='/shop'>
+			</OptionLink>
+			<OptionLink to='/shop'>
 				CONTACT
-			</Link>
+			</OptionLink>
 			{
 				currentUser ?
-				<div className='option' onClick={() => auth.signOut()}>
+				<OptionDiv onClick={() => auth.signOut()}>
 					SIGN OUT
-				</div>
+				</OptionDiv>
 				: 
-				<Link className='option' to='/signin'>
+				<OptionLink to='/signin'>
 					SIGN IN
-				</Link>
+				</OptionLink>
 			}
 			<CartIcon />
-		</div>
+		</OptionsContainer>
 		{ hidden ? null : <CartDropdown /> }
-	</div>
+	</HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
